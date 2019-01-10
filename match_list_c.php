482731fcabc3f3c845a78fe973a06c6da30657fe
zhiwei <750470183@qq.com>
2019-01-10 18:53:10 +0800

    新建控制器

---
+++ match_list_c.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-type:text/html;charset=utf8");//初始化
+
+
+//实例化相应的模型类对象，调用某个方法，实现固定功能
+require'./MatchModel.class.php';
+
+$m_match = new MatchModel();
+$match_list = $m_match->getlist();
+
+//载入负责显示的html文件
+require'./template/match_list_v.html';
+
+
+
+
+select * from 'match' as m left join '
